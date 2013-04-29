96bc1b21c217d007a3789cab005572b866b642f0
Lars Tingelstad <lars@larstech-UD3R.(none)>
2013-04-30 00:21:16 +0200

    added deployment script

---
+++ ur_deployment_test.lua
@@ -0,0 +1,34 @@
+
+tc = rtt.getTC()
+depl = tc:getPeer("Deployer")
+depl:import("ocl")
+
+depl:import("UniversalRobotsUR5")
+depl:loadComponent("ur5", "UniversalRobotsUR5")
+ur5 = depl:getPeer("ur5")
+
+depl:import("URRealTime")
+depl:loadComponent("urrtt", "URRealTime")
+urrtt = depl:getPeer("urrtt")
+
+urrtt:configure()
+ur5:configure()
+time_period = 1.0/60.0
+ur5:setPeriod(0.001)
+
+
+urrtt_q_inputport = urrtt:getPort("URRTTDesiredJointPosition")
+urrtt_q_outputport = urrtt:getPort("URRTTActualJointPosition")
+urrtt_qdot_outputport = urrtt:getPort("URRTTActualJointVelocity")
+
+
+ur5_q_inputport = ur5:getPort("UR5JointPosition")
+ur5_qdot_inputport = ur5:getPort("UR5JointVelocity")
+ur5_q_outputport = ur5:getPort("UR5DesiredJointPosition")
+
+
+urrtt_q_outputport:connect(ur5_q_inputport)
+ur5_q_outputport:connect(urrtt_q_inputport)
+
+urrtt:start()
+ur5:start()
